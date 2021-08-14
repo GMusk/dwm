b969293c05ad41134cc3bf50624a3d7edd22c443
George Musker <georgemusk@msn.com>
2021-08-14 18:32:18 +0100

    vanity gaps patch + config def tidy

--- config.def.h
+++ config.def.h
@@ -3,6 +3,11 @@
 /* appearance */
 static unsigned int borderpx  = 1;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char font[]            = "monospace:size=10";
@@ -56,12 +61,6 @@ static const Layout layouts[] = {
 #define STACKKEYS(MOD,ACTION) \
 	{ MOD, XK_j,     ACTION##stack, {.i = INC(+1) } }, \
 	{ MOD, XK_k,     ACTION##stack, {.i = INC(-1) } }, \
-	/*
-        { MOD, XK_grave, ACTION##stack, {.i = PREVSEL } }, \
-	{ MOD, XK_q,     ACTION##stack, {.i = 0 } }, \
-	{ MOD, XK_a,     ACTION##stack, {.i = 1 } }, \
-	{ MOD, XK_x,     ACTION##stack, {.i = -1 } },
-        */
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
@@ -129,6 +128,10 @@ STACKKEYS(MODKEY|ShiftMask,                                push)
 	{ MODKEY|ShiftMask,     XK_comma,                  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,     XK_period,                 tagmon,         {.i = +1 } },
 	{ MODKEY|ShiftMask,     XK_e,                      quit,           {0} },
+	{ MODKEY|Mod1Mask,              XK_h,      incrgaps,       {.i = +1 } },
+	{ MODKEY|Mod1Mask,              XK_l,      incrgaps,       {.i = -1 } },
+	{ MODKEY|Mod1Mask,              XK_0,      togglegaps,     {0} },
+	{ MODKEY|Mod1Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
 	{ 0,                    XK_Print,                  spawn,          SHCMD("flameshot gui") },
 	{ 0,                    XF86XK_MonBrightnessUp,    spawn,          SHCMD("xbacklight -inc 10") },
 	{ 0,                    XF86XK_MonBrightnessDown,  spawn,          SHCMD("xbacklight -dec 10") },
